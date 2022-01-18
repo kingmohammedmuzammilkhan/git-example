2239b7f4504483e39547fdd68df16711838c335c
kingmohammedmuzammilkhan <96713283+kingmohammedmuzammilkhan@users.noreply.github.com>
2022-01-18 14:44:46 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     // chage in local
     public static void main(String[] args) {
-        if(int n==2)System.out.println("server");
+        //System.out.println("server");
         System.out.println("mmk 2");
         
         
