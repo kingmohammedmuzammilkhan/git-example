577442e5c3468d2545e27bae8ed56d60082e8414
kingmohammedmuzammilkhan <96713283+kingmohammedmuzammilkhan@users.noreply.github.com>
2022-01-18 14:42:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 public class Hello{
     // chage in local
     public static void main(String[] args) {
+        System.out.println("server");
         System.out.println("mmk 2");
+        
         
         //git in server
     }
