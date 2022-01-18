eb5fe1e303530b670df6dabc6ea012a9c296ea53
Mohammed Muzammil Khan <raichur.muzammil@gmail.com>
2022-01-18 14:50:46 +0530

    if changed

--- Hello.java
+++ Hello.java
@@ -1,12 +1,9 @@
 public class Hello{
     // chage in local
     public static void main(String[] args) {
-<<<<<<< HEAD
         int n=0;
         if(n==2)System.out.println("server");
-=======
         //System.out.println("server");
->>>>>>> 2239b7f4504483e39547fdd68df16711838c335c
         System.out.println("mmk 2");
         
         
